748749e5fea61f5fc697852ec6414a2d887230b5
monicasunu <monica.j.sun@gmail.com>
2013-03-05 14:45:56 -0800

    Update query.rb

--- lib/query.rb
+++ lib/query.rb
@@ -104,6 +104,37 @@ class IDQuery < Query
 end
 
 =begin
+= Class: HighlightedQuery < Query
+  Sort results ascending or decending by a given field and hilight results
+
+== Parameters:
+  query::   The query text. Blank or * queries are converted to match_all
+  from::    Beginning of time range
+  to::      End of time range (Default: NOW)
+  offset::  Offset from beginning of results
+  field::   Field to sort on, be careful of fields with many unique values
+  order::   desc/asc
+=end
+class HighlightedQuery < Query
+  attr_accessor :query,:from,:to
+  def initialize(question, perms, from, to, offset = 0, size = KibanaConfig::Per_page, field = "@timestamp", order = "desc")
+    super(question, perms, from, to)
+    @query['from'] = offset
+    @query['size'] = size
+    @query['sort'] = {
+      field => {
+        "order" => order
+      }
+    }
+    @query['highlight'] = {
+      "pre_tags" => [ "@KIBANA_HIGHLIGHT_START@" ],
+      "post_tags" => [ "@KIBANA_HIGHLIGHT_END@" ],
+      "fields" => { KibanaConfig::Highlighted_field => { "fragment_size" => 9999 } }
+    }
+  end
+end
+
+=begin
 = Class: SortedQuery < Query
   Sort results ascending or decending by a given field
 
