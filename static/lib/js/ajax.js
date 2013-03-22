5fa1ee8ad0962649eea80a2a6ad5d84f8e2bd211
monicasunu <monica.j.sun@gmail.com>
2013-03-21 19:24:50 -0700

    Update ajax.js
    heatmap

--- static/lib/js/ajax.js
+++ static/lib/js/ajax.js
@@ -1060,7 +1060,7 @@ function mFields(field) {
 function feedLinks(obj) {
   return "<a href=report#" + Base64.encode(JSON.stringify(obj, null, '')) +">report " + 
     "<i class='icon-file'></i></a> " +
-    "<a href=heatMap#" + Base64.encode(JSON.stringify(obj, null, '')) + ">heatMap " + 
+    "<a href=heatMap/" + Base64.encode(JSON.stringify(obj, null, '')) + ">heatMap " + 
     "<i class='icon-globe'></i></a> " +
     "<a href=rss/" + Base64.encode(JSON.stringify(obj, null, '')) +">rss " +
     "<i class='icon-rss'></i></a> "+
